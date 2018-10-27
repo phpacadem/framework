3c85b7820964c604765b42ea67e140ed25d3fa7f
Ivan Tsimbalist <naborde@inbox.ru>
2018-10-27 15:32:00 +0700

    Fix router strategy for exceptions

--- src/route/strategy/ApplicationStrategy.php
+++ src/route/strategy/ApplicationStrategy.php
@@ -3,11 +3,47 @@
 namespace PhpAcadem\framework\route\strategy;
 
 
+use League\Route\Http\Exception\MethodNotAllowedException;
+use League\Route\Http\Exception\NotFoundException;
 use PhpAcadem\framework\middleware\ErrorHandlerMiddleware;
 use Psr\Http\Server\MiddlewareInterface;
 
 class ApplicationStrategy extends \League\Route\Strategy\ApplicationStrategy
 {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNotFoundDecorator(NotFoundException $exception): MiddlewareInterface
+    {
+        return $this->throwThrowableMiddleware($exception);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethodNotAllowedDecorator(MethodNotAllowedException $exception): MiddlewareInterface
+    {
+        return $this->throwThrowableMiddleware($exception);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getThrowableHandler(): MiddlewareInterface
+    {
+        $errorHandlerMiddleware = $this->getErrorHandlerMiddleware();
+        return $errorHandlerMiddleware;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExceptionHandler(): MiddlewareInterface
+    {
+        $errorHandlerMiddleware = $this->getErrorHandlerMiddleware();
+        return $errorHandlerMiddleware;
+    }
 
     protected function getErrorHandlerMiddleware()
     {
@@ -20,15 +56,6 @@ class ApplicationStrategy extends \League\Route\Strategy\ApplicationStrategy
     {
         $errorHandlerMiddleware = $this->getErrorHandlerMiddleware();
         $errorHandlerMiddleware->setException($error);
-        return $errorHandlerMiddleware;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getThrowableHandler(): MiddlewareInterface
-    {
-        $errorHandlerMiddleware = $this->getErrorHandlerMiddleware();
         return $errorHandlerMiddleware;
     }
 }
