da22b485430ee586bc022b363ad1d3917a3aeec5
Ivan Tsimbalist <naborde@inbox.ru>
2018-10-31 23:34:36 +0700

    View extentions

--- src/view/ViewEngineInterface.php
+++ src/view/ViewEngineInterface.php
@@ -3,11 +3,20 @@
 namespace PhpAcadem\framework\view;
 
 
+use League\Plates\Extension\ExtensionInterface;
+
 interface ViewEngineInterface
 {
     public function addData(array $data, $templates = null);
 
     public function getData($template = null);
+
+    /**
+     * Load an extension.
+     * @param  ExtensionInterface $extension
+     * @return ViewEngineInterface
+     */
+    public function loadExtension(ExtensionInterface $extension);
 
     public function render($name, array $data = array());
 }
